17d539882d87f50e536e1f8522fae4960c5c3aa2
Wenjun Shao <v-wenjunshao@microsoft.com>
2026-06-16 14:12:15 +0800

    7

--- init.sh
+++ init.sh
@@ -11,15 +11,58 @@ echo "[INFO] Current user: $USER"
 
 # ===== mapping relationship =====
 case $USER in
-  "LabRunnerPPE1@MngEnvMCAP825705.onmicrosoft.com"
+    "LabRunner1@MngEnvMCAP825705.onmicrosoft.com")
+    SUB_ID="8da443ca-8590-4e4c-bc26-defea6467f50"
+    ;;
+    "LabRunner2@MngEnvMCAP825705.onmicrosoft.com")
+    SUB_ID="8da443ca-8590-4e4c-bc26-defea6467f50"
+    ;;
+    "LabRunner3@MngEnvMCAP825705.onmicrosoft.com")
+    SUB_ID="8da443ca-8590-4e4c-bc26-defea6467f50"
+    ;;
+    "LabRunner4@MngEnvMCAP825705.onmicrosoft.com")
+    SUB_ID="8da443ca-8590-4e4c-bc26-defea6467f50"
+    ;;
+    "LabRunner5@MngEnvMCAP825705.onmicrosoft.com")
+    SUB_ID="8da443ca-8590-4e4c-bc26-defea6467f50"
+    ;;
+    "LabRunner6@MngEnvMCAP825705.onmicrosoft.com")
+    SUB_ID="8da443ca-8590-4e4c-bc26-defea6467f50"
+    ;;
+
+    "LabRunnerPPE1@MngEnvMCAP825705.onmicrosoft.com")
+    SUB_ID="e90f488d-02c7-4c79-a332-523afd8d1a44"
+    ;;
+    "LabRunnerPPE2@MngEnvMCAP825705.onmicrosoft.com")
+    SUB_ID="e90f488d-02c7-4c79-a332-523afd8d1a44"
+    ;;
+    "LabRunnerPPE3@MngEnvMCAP825705.onmicrosoft.com")
+    SUB_ID="e90f488d-02c7-4c79-a332-523afd8d1a44"
+    ;;
+    "LabRunnerPPE4@MngEnvMCAP825705.onmicrosoft.com")
     SUB_ID="e90f488d-02c7-4c79-a332-523afd8d1a44"
     ;;
 
-  "wenjun@MngEnvMCAP825705.onmicrosoft.com"
+    "LabRunnerDevPPE1@MngEnvMCAP825705.onmicrosoft.com")
+    SUB_ID="d470c937-42bc-4a9c-9831-116799e343d9"
+    ;;
+    "LabRunnerDevPPE2@MngEnvMCAP825705.onmicrosoft.com")
+    SUB_ID="d470c937-42bc-4a9c-9831-116799e343d9"
+    ;;  
+    "LabRunnerDevPPE3@MngEnvMCAP825705.onmicrosoft.com")
+    SUB_ID="d470c937-42bc-4a9c-9831-116799e343d9"
+    ;; 
+    "LabRunnerDevPPE4@MngEnvMCAP825705.onmicrosoft.com")
+    SUB_ID="d470c937-42bc-4a9c-9831-116799e343d9"
+    ;; 
+
+  
+
+  "wenjun@MngEnvMCAP825705.onmicrosoft.com")
     SUB_ID="d470c937-42bc-4a9c-9831-116799e343d9"
     ;;
 
-  *
+  *)
     echo "[ERROR] Unknown account: $USER"
     exit 1
     ;;
